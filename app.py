fb4d407cafa601e84ab672caca5d71cbbc3a4e1b
wolwesking <takacspatrik993@gmail.com>
2023-01-18 21:00:54 +0500

    Finished function: background removing

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, request
-from fileinput import filename
-from werkzeug.utils import secure_filename
-import os
+# Backend imports
+from flask import Flask, render_template, request, url_for # BSD-3-Clause license, open-source: https://github.com/pallets/flask
+import time # default libary
 
+# Progress imports
+from rembg import remove # MIT, open-source: https://github.com/danielgatis/rembg
+from PIL import Image # Python Software Foundation License, open-source: https://github.com/python-pillow/Pillow
+
+# Global variables
 app = Flask(__name__)
 
-#Funcntions
+# Funcntions
+def removeBG(file):
+    input = Image.open(file)
+    output = remove(input)
+    newFilename = f"static/outputs/{str(int(time.time()))}.png"
+    output.save(newFilename)
+    return newFilename
 
-# Routes
+# def clearUpFiles(fileName):
+#     return #none
+
+# ----- Routes -----
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
@@ -17,8 +30,8 @@ def upload():
     if request.method == 'POST':
         
         # Allowed extenctions
-        UPLOAD_FOLDER = './uploads'
-        ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+        UPLOAD_FOLDER = './outputs'
+        ALLOWED_EXTENSIONS = ['.png', '.jpg', '.jpeg']
 
         app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -35,9 +48,8 @@ def upload():
         # If file is secure
         # TODO Allowed file and allowed_file(file.filename)
         if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('Acknowledgement.html', name = filename)
+            filePath = removeBG(file)
+            return render_template('Acknowledgement.html', name = filePath)
 
 # END
 if __name__ =="__main__":
